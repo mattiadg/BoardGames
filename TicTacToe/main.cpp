ebc984e2b43c0b7ab4d3b991d83293b87b6fe9c1
Alex Wasey <43968667+alexanderwasey@users.noreply.github.com>
2018-10-10 08:42:35 +0100

    Game no longer exits on error (#10)

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -10,7 +10,13 @@ int main() {
         char curr_sym = game.getTurn() % 2 == 0 ? 'x' : 'o';
         std::cout << game.getTurn() << std::endl;
         display(std::cout, d);
-        d.makeMove(std::cin, curr_sym);
+        try{
+          d.makeMove(std::cin, curr_sym);
+        }
+        catch(std::invalid_argument e){
+            std::cout << "Invalid input!\n";
+            std::cout << e.what() << '\n';
+        }
     }
     //TicTacToeBoard b2(std::string(""));
     //TicTacToeBoard(std::string("xox..cxox"));
