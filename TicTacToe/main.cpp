e1eb3c8086e3d4df5ce02b7066671820b78f2d7d
Pansaai <pans.thomas@gmail.com>
2018-10-11 15:57:42 +0200

    Informative outputs during game #6

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -8,22 +8,42 @@ int main() {
     TicTacToe game;
     TicTacToeDisplay d(game);
     TicTacToePlayer engine('o');
+
+	// Goal of the game
+	std::cout << "GOAL OF THE GAME:" << std::endl;
+	std::cout << std::endl;
+	std::cout << "The goal of the game is to get 3 of your symbols in a row. Be it horizontally, vertically or diagonally." << std::endl;
+	std::cout << std::endl;
+
+	// Instructions
+	std::cout << "INSTRUCTIONS:" << std::endl;
+	std::cout << std::endl;
+	std::cout << "When prompted give 2 numbers between 0 and 2, which represent the row and column." << std::endl;
+	std::cout << std::endl;
+	std::cout << "USAGE\tGive your next move (row, col): [row] [col]" << std::endl;
+
     while(game.getBoard().getGameState() == Continue) {
         auto turn = game.getTurn();
+		std::cout << std::endl;
         std::cout << "Turn: " << turn << std::endl;
         display(std::cout, d);
         char curr_sym = turn % 2 == 0 ? 'x' : 'o';
         if (turn % 2 == 0) {
             try {
-                d.makeMove(std::cin, curr_sym);
+				std::cout << "It is your turn." << std::endl;
+				std::cout << "Give your next move (row, col): ";
+				d.makeMove(std::cin, curr_sym);
+				std::cout << std::endl;
             }
             catch (std::invalid_argument e) {
                 std::cout << "Invalid input!\n";
                 std::cout << e.what() << '\n';
             }
         } else {
+			std::cout << "It is the computers turn." << std::endl;
             auto engine_move = engine.play(game.getBoard());
             game.makeMove(engine_move);
+			std::cout << std::endl;
         }
     }
     return 0;
